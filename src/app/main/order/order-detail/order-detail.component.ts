2a4bb2fcfbdf0a4532621f231f3c196389accfb1
TUANPH-HOME-PC\TUANPH-PC <tuanpham@blogicsystems.com>
2017-08-02 03:21:01 +0700

    Should inscrase more row if total items >16

--- src/app/main/order/order-detail/order-detail.component.ts
+++ src/app/main/order/order-detail/order-detail.component.ts
@@ -4,6 +4,7 @@ import { NotificationService } from '../../../core/services/notification.service
 import { UtilityService } from '../../../core/services/utility.service';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { SystemConstants } from '../../../core/common/system.constants';
 
 @Component({
   selector: 'app-order-detail',
@@ -14,15 +15,16 @@ export class OrderDetailComponent implements OnInit {
   public orderDetails: any[];
   public entity: any;
   public totalAmount: number;
+  public orderId: number;
+  public baseFolder: string = SystemConstants.BASE_API;
   constructor(private _utilityService: UtilityService,
     private _dataService: DataService,
     private _activatedRoute: ActivatedRoute,
     private _notificationService: NotificationService) { }
 
-
   ngOnInit() {
     this._activatedRoute.params.subscribe((params: Params) => {
-      let orderId = params['id'];
+      this.orderId = params['id'];
       this.loadOrder(params['id']);
 
       this.loadOrderDetail(params['id']);
@@ -44,6 +46,15 @@ export class OrderDetailComponent implements OnInit {
       }
       console.log(this.totalAmount);
     }, error => this._dataService.handleError(error));
+  }
+  public exportToExcel() {
+    this._dataService.get('/api/order/exportExcel/' + this.orderId.toString())
+      .subscribe((response: any) => {
+        window.open(this.baseFolder + response.Message);
+      })
+      , ((error: any) => {
+        this._dataService.handleError(error);
+      });
   }
   public goBack() {
     this._utilityService.navigate('/main/order');
