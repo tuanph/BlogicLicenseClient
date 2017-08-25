bfe8b164a43ad6f34bd8462af997131f46d9c49c
tuanph <tuanpham@blogicsystems.com>
2017-08-25 16:07:05 +0700

    Fix ugly bug when show model. Import script ngOnit main component instead app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,14 @@
-import { Component, AfterViewChecked, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent {
   title = 'app';
 
-  constructor(private elementRef: ElementRef) {
-
-  }
-
-  ngAfterViewChecked() {
-    var existsScript = document.getElementById("customJS");
-    if (existsScript != null) {
-      this.elementRef.nativeElement.removeChild(existsScript);
-    }
-    else {
-      var cusScript = document.createElement("script");
-      cusScript.type = "text/javascript";
-      cusScript.src = "../assets/js/custom.js";
-      cusScript.id = "customJS";
-      this.elementRef.nativeElement.appendChild(cusScript);
-    }
+  constructor() {
   }
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit,ElementRef } from '@angular/core';
 import { AuthenService } from '../core/services/authen.service';
 import { LoggedInUser } from '../core/model/loggedin.user';
 import { SystemConstants } from '../core/common/system.constants';
@@ -9,12 +9,26 @@ import { SystemConstants } from '../core/common/system.constants';
 })
 export class MainComponent implements OnInit {
   public user: LoggedInUser;
-  constructor(private _authenService: AuthenService) { }
+  constructor(private _authenService: AuthenService,private elementRef: ElementRef) { }
   public baseFolder: string = SystemConstants.BASE_API;
   ngOnInit() {
     this.user = this._authenService.getLoginUser();
+
+     var existsScript = document.getElementById("customJS");
+    if (existsScript != null) {
+      this.elementRef.nativeElement.removeChild(existsScript);
+    }
+    else {
+      var cusScript = document.createElement("script");
+      cusScript.type = "text/javascript";
+      cusScript.src = "../assets/js/custom.js";
+      cusScript.id = "customJS";
+      this.elementRef.nativeElement.appendChild(cusScript);
+    }
   }
   logout(){
     this._authenService.logout();
   }
+
+
 }
